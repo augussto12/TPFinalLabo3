948e854ad6af62e9948a6f00dbe05461d1b2f82d
augussto12 <augustoalvarez10@gmail.com>
2025-06-03 21:29:59 -0300

    Validacion parcial de la fecha a ingresar

--- src/clasesManejoTurnos/Turno.java
+++ src/clasesManejoTurnos/Turno.java
@@ -1,8 +1,10 @@
 package clasesManejoTurnos;
+
 import clasesPersonas.Medico;
 import clasesPersonas.Paciente;
 import clasesPersonas.Persona;
 
+import java.time.DateTimeException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
@@ -17,8 +19,7 @@ public class Turno {
     private int idTurno;
 
 
-
-    public Turno (LocalDateTime fecha, Medico medico, Paciente cliente, String motivo,int idTurno) {
+    public Turno(LocalDateTime fecha, Medico medico, Paciente cliente, String motivo, int idTurno) {
         this.fecha = fecha;
         this.medico = medico;
         this.cliente = cliente;
@@ -54,32 +55,60 @@ public class Turno {
         this.motivo = motivo;
     }
 
-    public int getIdTurno() {return idTurno;}
+    public int getIdTurno() {
+        return idTurno;
+    }
 
 
-    public static LocalDateTime llenarFecha()
-    {
+    public static LocalDateTime llenarFecha() {
         Scanner scan = new Scanner(System.in);
-        System.out.println("Fecha para cuando quiere sacar turno");
-        //Agenda agenda = new Agenda();
-        System.out.printf("\nAnio: ");
-        int anio = scan.nextInt();
-        scan.nextLine();
-        System.out.printf("\nMes: ");
-        int mes = scan.nextInt();
-        scan.nextLine();
-        System.out.printf("\nDia: ");
-        int dia = scan.nextInt();
-        scan.nextLine();
-        System.out.printf("\nHora: ");
-        int hora = scan.nextInt();
-        scan.nextLine();
-        System.out.printf("\nMinuto: ");
-        int minuto = scan.nextInt();
-        scan.nextLine();
-        LocalDateTime fecha = LocalDateTime.of(anio, mes, dia, hora, minuto);
+        boolean fechaValida = false;
+        LocalDateTime fecha = null;
+
+        while (!fechaValida) {
+            try {
+                System.out.println("Fecha para cuando quiere sacar turno");
+                //Agenda agenda = new Agenda();
+                System.out.printf("\nAnio: ");
+                int anio = scan.nextInt();
+                scan.nextLine();
+                System.out.printf("\nMes: ");
+                int mes = scan.nextInt();
+                scan.nextLine();
+                System.out.printf("\nDia: ");
+                int dia = scan.nextInt();
+                scan.nextLine();
+                System.out.printf("\nHora: ");
+                int hora = scan.nextInt();
+                scan.nextLine();
+                System.out.printf("\nMinuto: ");
+                int minuto = scan.nextInt();
+                scan.nextLine();
+                fecha = LocalDateTime.of(anio, mes, dia, hora, minuto);
+
+                if (fecha.isBefore(LocalDateTime.now())) {
+                    System.out.println("La fecha no puede ser antes de hoy.");
+                } else {
+                    fechaValida = true;
+                }
+            } catch (DateTimeException e) {
+                System.out.println("Fecha invalida. Ingrese otra fecha.");
+                scan.nextLine();
+            }
+
+        }
         return fecha;
     }
+
+    public static boolean fechaDisponible(LocalDateTime fecha, List<Turno> turnos) {
+        for (Turno t : turnos) {
+            if (fecha.equals(t.getFecha())) {
+                return false;
+            }
+        }
+        return true;
+    }
+
 
     @Override
     public String toString() {
